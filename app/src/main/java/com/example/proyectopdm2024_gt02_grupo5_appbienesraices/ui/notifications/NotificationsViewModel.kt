8807473bdd8dd4eaf14b13ae3ccfd694cdca6e4b
Mauricio Isai Valdez Hernandez <mvaldez@omnisport.com>
2024-04-20 15:25:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/proyectopdm2024_gt02_grupo5_appbienesraices/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.proyectopdm2024_gt02_grupo5_appbienesraices.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoPDM2024-GT02-Grupo5-AppBienesRaices"
+include(":app")
+ 
