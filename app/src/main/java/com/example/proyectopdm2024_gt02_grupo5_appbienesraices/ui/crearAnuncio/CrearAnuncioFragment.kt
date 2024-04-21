cf46e173db9f97b5e96509f1fe9d9c1bbcd39bcb
mauriisai <mauriisai@hotmail.com>
2024-04-21 00:45:21 -0600

    creacion de CrearAnuncioFragment

--- app/src/main/java/com/example/proyectopdm2024_gt02_grupo5_appbienesraices/ui/crearAnuncio/CrearAnuncioFragment.kt
+++ app/src/main/java/com/example/proyectopdm2024_gt02_grupo5_appbienesraices/ui/crearAnuncio/CrearAnuncioFragment.kt
@@ -1,4 +1,4 @@
-package com.example.proyectopdm2024_gt02_grupo5_appbienesraices.ui.dashboard
+package com.example.proyectopdm2024_gt02_grupo5_appbienesraices.ui.crearAnuncio
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.proyectopdm2024_gt02_grupo5_appbienesraices.databinding.FragmentDashboardBinding
+import com.example.proyectopdm2024_gt02_grupo5_appbienesraices.databinding.FragmentCrearAnuncioBinding
 
-class DashboardFragment : Fragment() {
+class CrearAnuncioFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentCrearAnuncioBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,15 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val crearAnuncioViewModel =
+            ViewModelProvider(this).get(CrearAnuncioViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentCrearAnuncioBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        crearAnuncioViewModel.text.observe(viewLifecycleOwner) {
+
         }
         return root
     }
