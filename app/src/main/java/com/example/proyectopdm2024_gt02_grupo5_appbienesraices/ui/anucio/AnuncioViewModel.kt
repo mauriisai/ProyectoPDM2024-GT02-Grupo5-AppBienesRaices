3fd7df6582eb319ca5b4854c1b7bc835b9bd58b2
noelia <noeliaargueta6@gmail.com>
2024-04-21 13:13:28 -0600

    creacion de Anuncio fragment

---
+++ app/src/main/java/com/example/proyectopdm2024_gt02_grupo5_appbienesraices/ui/anucio/AnuncioViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.proyectopdm2024_gt02_grupo5_appbienesraices.ui.anucio
+
+import androidx.lifecycle.ViewModel
+
+class AnuncioViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
